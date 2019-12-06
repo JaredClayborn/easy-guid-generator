d2210a0067f2c5e12dbc3a54b1ddc2401908978a
JaredClayborn <coder-guy@yandex.com>
2019-12-06 16:12:14 -0600

    update exports

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-module.exports = {
-    generateGuid: (brackets) => {
-        const sizes = [8,4,4,4,12]
-        const possible = 'ABCDEF0123456789'
-        let guid = ''
-        sizes.forEach((size, index) => {
-            if (index !== 0) {
-                guid += '-'
-            }
-            for (let i = 0; i < size; i++) {
-                guid += possible[Math.floor(Math.random() * 16)]
-            }
-        })
-
-        return brackets === true ? `{${guid}}` : guid
-    },
-
-    emptyGuid: (brackets) => {
-        let emptyGuid = '00000000-0000-0000-0000-000000000000'
-        return brackets === true ? `{${emptyGuid}}` : emptyGuid
-    },
-
-    generateMultipleGuids: (numToGen, brackets) => {
-        let guids = []
-        for (let i = 0; i < numToGen; i++) {
-            guids.push(this.generateGuid(brackets))
+exports.generateGuid = function (brackets) {
+    const sizes = [8, 4, 4, 4, 12]
+    const possible = 'ABCDEF0123456789'
+    let guid = ''
+    sizes.forEach((size, index) => {
+        if (index !== 0) {
+            guid += '-'
         }
+        for (let i = 0; i < size; i++) {
+            guid += possible[Math.floor(Math.random() * 16)]
+        }
+    })
 
-        return guids
+    return brackets === true ? `{${guid}}` : guid
+}
+
+exports.emptyGuid = function (brackets) {
+    let emptyGuid = '00000000-0000-0000-0000-000000000000'
+    return brackets === true ? `{${emptyGuid}}` : emptyGuid
+}
+
+
+exports.generateMultipleGuids = (numToGen, brackets) {
+    let guids = []
+    for (let i = 0; i < numToGen; i++) {
+        guids.push(generateGuid(brackets))
     }
+
+    return guids
+}
 }
